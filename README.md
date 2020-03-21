c1ad50617babe987acdc7446e0a11a4116564bde
Ethan Smith <smith.11411@osu.edu>
2020-03-21 12:41:31 -0700

    New strings, better README, and updated dependencies - oh my

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
-# MitchBot 
-###### Add :8ball::zap: to your Discord server
+# MitchBot
+
+## Add :8ball::zap: to your Discord server
+
+### Overview
+
+This is a simple Discord bot that listens for specific messages. When it sees a trigger message, it responds with a random phrase from a list of strings. That's it... that's the repo.
+
+### Usage
+
+#### 1) Replace the trigger message
+
+```js
+discordClient.on('message', msg => {
+        if (msg.content === 'TRIGGER') {
+            msg.channel.send(getRandomMitchism());
+        }
+    });
+```
+
+#### 2) Set response strings
+
+```json
+{
+    "strings": [
+        "You can add whatever you want here",
+        "Lorem ipsum",
+        "Et cetera"
+    ]
+}
+```
+
+#### 3) Create a .env file and add your Discord BOT_TOKEN
+
+```text
+BOT_TOKEN=<your token>
+```
 
 ## Acknowledgements
 
 Special thanks to my friend Mitch for giving me permission to make this bot.
-
-## Contributors
-
-- Ethan Smith
--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 /* 
 index.js
-mitchbot for Discord
+mitchbot for Discord and GroupMe
 Ethan Smith
 */
 
 require('dotenv').config();
+const Express = require('express');
 const Discord = require('discord.js');
 const fs = require('fs');
-const client = new Discord.Client();
 
-let strings = fs.readFileSync('strings.json');
-const mitchisms = JSON.parse(strings).strings;
+let mitchismsList = fs.readFileSync('strings.json');
+const mitchisms = JSON.parse(mitchismsList).strings;
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-});
+function getRandomMitchism() {
+    return mitchisms[Math.floor(Math.random() * mitchisms.length)];
+}
 
-client.on('message', msg => {    
-    if (msg.content === '8ball') {
-        let randomMitchism = mitchisms[Math.floor(Math.random() * mitchisms.length)];
-        msg.channel.send(randomMitchism);
-    } else if (msg.content === '🎱') {
-        msg.channel.send('I will kill you all');
-    }
-});
+function initBots() {
+    initDiscord(new Discord.Client());
+    initGroupme(Express());
+}
 
-client.login(process.env.BOT_TOKEN);
+function initDiscord(discordClient) {
+    discordClient.on('ready', () => {
+        console.log(`Logged in as ${discordClient.user.tag}!`)
+    });
+    
+    discordClient.on('message', msg => {
+        if (msg.content === '8ball') {
+            msg.channel.send(getRandomMitchism());
+        } else if (msg.content === '🎱') {
+            msg.channel.send('I will kill you all');
+        }
+    });
+    
+    discordClient.login(process.env.BOT_TOKEN);
+}
+
+function initGroupme(groupmeClient) {
+    const port = 3000;
+    let mitchbotResponse = {
+        'bot_id'  : process.env.TEST_GROUPME_TOKEN,
+        'text'    : 'Something went wrong... Please contact Wheat Thin'
+    };
+
+    // groupmeClient.use(Express.bodyParser());
+    groupmeClient.listen(port, () => {console.log('Listening on port', port)});
+
+    groupmeClient.post('/', (request, response) => {
+        console.log(request.body);
+        if (request.body.text === '8ball') {
+            mitchbotResponse.text = getRandomMitchism();
+            response.send(mitchbotResponse);
+        } else {
+            response.send('Something went wrong');
+        }
+    });
+}
+
+initBots();
